0dc28b47592907e327ac88a4080badc04bc50944
tkostadinov004 <tkostadinov04@gmail.com>
2026-03-31 00:36:10 +0300

    fixed case in names

--- issuetracker-backend/src/main/java/bg/sofia/uni/fmi/issuetracker/IssueTrackerApplication.java
+++ issuetracker-backend/src/main/java/bg/sofia/uni/fmi/issuetracker/IssueTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IssuetrackerApplication {
+public class IssueTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IssuetrackerApplication.class, args);
+		SpringApplication.run(IssueTrackerApplication.class, args);
 	}
 
 }
