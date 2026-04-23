00d48eac58ef4586d2afd9eb2ee4f2cd058b35e9
tkostadinov004 <tkostadinov04@gmail.com>
2026-04-23 12:12:30 +0300

    fixed jpql bug

--- issuetracker-backend/src/main/java/bg/sofia/uni/fmi/issuetracker/repository/ProjectRepository.java
+++ issuetracker-backend/src/main/java/bg/sofia/uni/fmi/issuetracker/repository/ProjectRepository.java
@@ -18,7 +18,7 @@ public interface ProjectRepository extends JpaRepository<Project, String> {
             "where pu.project = :project and pu.user = :user and pu.id.role in :roles")
     boolean hasRoles(@Param("user") User user, @Param("project") Project project, @Param("roles") Iterable<Role> roles);
 
-    @Query("select count(pu.id) = :#{roles.size()} " +
+    @Query("select count(pu.id) = :#{#roles.size()} " +
             "from ProjectUser pu " +
             "where pu.project = :project and pu.user = :user and pu.id.role in :roles")
     boolean hasRolesStrict(@Param("user") User user, @Param("project") Project project, @Param("roles") Collection<Role> roles);
