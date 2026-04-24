d348e948058aa5d2e449c6cf41c2361e0a123b3b
tkostadinov004 <tkostadinov04@gmail.com>
2026-04-24 17:05:11 +0300

    removed unnecessary repo method

--- issuetracker-backend/src/main/java/bg/sofia/uni/fmi/issuetracker/repository/TokenRepository.java
+++ issuetracker-backend/src/main/java/bg/sofia/uni/fmi/issuetracker/repository/TokenRepository.java
@@ -6,12 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, String> {
-    Optional<Token> findTokenByTokenValue(String tokenValue);
-
     List<Token> findAllByUser(User user);
 
     void deleteAllByUser(User user);
