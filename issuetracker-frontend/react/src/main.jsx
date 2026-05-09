fc9650b69dc08529ebefde8ad7169008445f6742
Aleksandar <amkanev@uni-sofia.bg>
2026-05-09 13:47:42 +0300

    Refactor: Convert to JS, fix structure, and setup routing

--- issuetracker-frontend/react/src/main.jsx
+++ issuetracker-frontend/react/src/main.jsx
@@ -3,20 +3,20 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 
-import App from './App.tsx'
-import Register from './Register.tsx'
+import App from './App.jsx'
+import Register from './components/Register.jsx'
 
 //From documentation for react-routing https://reactrouter.com/start/declarative/routing
 //I will be using the Declarative type of routing, because it's the base one
 
 const root = document.getElementById('root');
 
-createRoot(root!).render(
+createRoot(root).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}></Route>
-        <Route path="/try" element={<Register />}></Route>
+        <Route path="/register" element={<Register />}></Route>
         
       </Routes>
     </BrowserRouter>
