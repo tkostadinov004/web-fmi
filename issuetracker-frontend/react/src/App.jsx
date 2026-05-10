f6a8a27dfb2f466b2d2bc5510c42062a7b4735ed
Slavi123456 <74818665+Slavi123456@users.noreply.github.com>
2026-05-10 12:14:38 +0300

    Frontend/ Register page fixes - more fields on the form and css changes (#19)

--- issuetracker-frontend/react/src/App.jsx
+++ issuetracker-frontend/react/src/App.jsx
@@ -1,10 +1,14 @@
+import { NavLink } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <div className="app-container">
       <h1>Issue Tracker Project</h1>
-      
+    
+      <button>
+        <NavLink to="/register"> To Register page</NavLink>
+      </button>
     </div>
   )
 }
