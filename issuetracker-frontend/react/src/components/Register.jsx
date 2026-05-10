aa2cdc0adf9bcc64dc9c8de78634021f3cbe689d
Slavi123456 <74818665+Slavi123456@users.noreply.github.com>
2026-05-10 12:04:41 +0300

    Frontend/ Register page fixes - more fields on the form and css changes

--- issuetracker-frontend/react/src/components/Register.jsx
+++ issuetracker-frontend/react/src/components/Register.jsx
@@ -1,55 +1,66 @@
-import './Register.css'
+import "./Register.css";
 
 function Register() {
-    return (<>
-        <div className="auth-container">
-            <div className="auth-left">
-                <div className="auth-left-content">
-                    {/* This should be changed to NavLink */}
-                    <p className="back-link">← Home Page</p> 
+  return (
+    <>
+      <div className="auth-container">
+        <div className="card">
+          <div className="auth-left">
+            <div className="auth-left-content">
+              <h1>Get Started</h1>
+              <p>Already have an account?</p>
 
-                    <h1>Get Started</h1>
-                    <p>Already have an account?</p>
-
-                    <button className="login-btn">Log in</button>
-                </div>
+              <button className="login-btn">Log in</button>
             </div>
+          </div>
 
-            <div className="auth-right">
-                <div className="form-card">
-                    <div className="form-header">
-                        <span>Need help?</span>
-                    </div>
+          <div className="auth-right">
+            <div className="form-card">
+              <h2>Create account</h2>
 
-                    <h2>Create account</h2>
+              <form>
+                <div className="form-row">
+                  <div className="input-group">
+                    <label>First Name</label>
+                    <input type="text" name="firstName" />
+                  </div>
 
-                    <form>
-                        <label>Email</label>
-                        <input type="email" placeholder="Enter your email" />
-
-                        <label>Password</label>
-                        <input type="password" placeholder="Enter your password" />
-
-                        <label>Full Name</label>
-                        <input type="text" placeholder="Enter your name" />
-
-                        <div className="checkbox">
-                            <input type="checkbox" id="terms" />
-                            <label htmlFor="terms">
-                                I accept the terms of the agreement
-                            </label>
-                        </div>
-                        {/* Here also should have a NavLink tag for the transition */}
-                        <button type="submit" className="signup-btn">
-                            Sign up
-                        </button>
-                    </form>
+                  <div className="input-group">
+                    <label>Last Name</label>
+                    <input type="text" name="lastName" />
+                  </div>
                 </div>
+
+                  <div className="input-group">
+                    <label>Username</label>
+                    <input type="text" name="username" />
+                  </div>
+
+                  <div className="input-group">
+                    <label>Company Name</label>
+                    <input type="text" name="companyName" />
+                  </div>
+
+                <div className="input-group">
+                  <label>Email</label>
+                  <input type="email" name="email" />
+                </div>
+
+                <div className="input-group">
+                  <label>Password</label>
+                  <input type="password" name="password" />
+                </div>
+                {/* This should have NavLink tag for the transition to the login page */}
+                <button type="submit" className="signup-btn">
+                  Sign up
+                </button>
+              </form>
             </div>
+          </div>
         </div>
-
-
-    </>)
+      </div>
+    </>
+  );
 }
 
-export default Register
+export default Register;
